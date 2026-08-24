26b65413cfd7b46f585982a22ee7e3fe93a89dbd
Will8.Chen <Will8.Chen@advantech.com>
2026-08-24 11:46:22 +0800

    update for compile kernel v5 + xenomai

---
+++ build/download_dovetail.sh
@@ -0,0 +1,1 @@
+git clone --depth 1 -b v5.15.y-dovetail https://source.denx.de/Xenomai/linux-dovetail.git
---
+++ build/patch_xeno3toLinux.sh
@@ -0,0 +1,5 @@
+cd build/xenomai
+
+./scripts/prepare-kernel.sh \
+    --linux=../linux-dovetail \
+    --arch=x86 
