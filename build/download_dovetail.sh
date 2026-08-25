4c0ba08db0502556b5e99221cec120aef2d95d59
Will8.Chen <Will8.Chen@advantech.com>
2026-08-25 14:14:01 +0800

    update for support kernel v6.8 xenomai

--- build/download_dovetail.sh
+++ build/download_dovetail.sh
@@ -1,1 +1,7 @@
+# kernel version v5.15.204
 git clone --depth 1 -b v5.15.y-dovetail https://source.denx.de/Xenomai/linux-dovetail.git
+
+# kernel version v6.8
+git clone --depth 1 -b v6.8-dovetail-rebase https://gitlab.com/Xenomai/linux-dovetail.git
+
+
